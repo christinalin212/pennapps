9aa218167bfbbbc1ddec0963f3915b1f7bb30428
Scott Phillips <s.phill99@gmail.com>
2019-09-07 23:41:35 -0400

    changing the looks of the page and added pages

--- Textbook/AddUserViewController.swift
+++ Textbook/AddUserViewController.swift
@@ -18,6 +18,7 @@ class AddUserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         
         // Do any additional setup after loading the view.
     }
